576e43e65a0718b3fcd56969f363c2e7c29059a7
Ahmed Alhashimi <alhashimi346@gmail.com>
2017-06-11 15:28:10 -0700

    add create method to notes controller

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -11,35 +11,7 @@ class Api::NotesController < ApplicationController
     end
   end
 
-  def destroy
-    @note = Note.find(params[:id])
-    if @note.author_id == current_user.id
-      @note.destroy!
-      render :show
-    else
-      render json: {}, status: 403
-    end
-
-  end
-
-  def index
-    @notes = current_user.notes
-  end
-
-  def show
-    @note = Note.find(params[:id])
-  end
-
-  def update
-    @note = Note.find(params[:id])
-
-    if @note.update(note_params) && @note.author_id == current_user.id
-      render :show
-    else
-      render json: @note.errors.full_messages, status: 422
-    end
-
-  end
+  
 
   private
 
