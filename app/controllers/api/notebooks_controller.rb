47ba77070e7866a30ebf9a442d3c0bd7fcff3ded
Ahmed Alhashimi <alhashimi346@gmail.com>
2017-06-10 19:18:34 -0700

    show notebooks with index

--- app/controllers/api/notebooks_controller.rb
+++ app/controllers/api/notebooks_controller.rb
@@ -1,5 +1,51 @@
 class Api::NotebooksController < ApplicationController
   before_action :require_signed_in
 
+  def create
+    @notebook = Notebook.new(notebook_params)
+    @notebook.author_id = current_user.id
+    if @notebook.save
+      render :show
+    else
+      render json: @notebook.errors.full_messages, status: 422
+    end
+  end
 
+  def destroy
+    @notebook = Notebook.find(params[:id])
+    unless lastNotebook? || @notebook.author_id != current_user.id
+      @notebook.destroy!
+      render :show
+    else
+      render json: {}, status: 403
+    end
+  end
+
+  def index
+    @notebooks = current_user.notebooks
+  end
+
+  def show
+    @notebook = Notebook.find(params[:id])
+  end
+
+  def update
+    @notebook = Notebook.find(params[:id])
+
+    if @notebook.update(notebook_params) && @notebook.author_id == current_user.id
+      render :show
+    else
+      render json: @notebook.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def lastNotebook?
+    current_user.notebooks.length == 1
+  end
+
+  def notebook_params
+    params.require(:notebook).permit(:title)
+  end
 end
