8eeeb7bf56bed4562892835c2cb222f853958a5c
Ahmed Alhashimi <alhashimi346@gmail.com>
2017-06-11 18:24:16 -0700

    add validations to note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,7 +3,9 @@ class Note < ActiveRecord::Base
   include PgSearch
   multisearchable against: [:title, :body]
 
-
+  validates :title, :author_id, :notebook_id, presence: true
+  belongs_to :author, class_name: "User"
+  belongs_to :notebook
 
 
 end
