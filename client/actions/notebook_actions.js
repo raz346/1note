9663de014dd563cceb2c3289085d0d2897d14adc
Ahmed Alhashimi <alhashimi346@gmail.com>
2017-06-14 19:44:58 -0700

    add search and notebook action

---
+++ client/actions/notebook_actions.js
@@ -0,0 +1,37 @@
+var AppDispatcher = require('../dispatcher/dispatcher'),
+    NotebookConstants = require('../constants/notebook_constants');
+
+var NotebookActions = {
+    receiveAllNotebooks: function(notebooks) {
+        AppDispatcher.dispatch({
+            actionType: NotebookConstants.RECEIVE_ALL_NOTEBOOKS,
+            notebooks: notebooks
+        });
+    },
+    receiveCurrentNotebook: function(notebook) {
+        AppDispatcher.dispatch({
+            actionType: NotebookConstants.RECEIVE_CURRENT_NOTEBOOK,
+            notebook: notebook
+        });
+    },
+    removeNotebook: function(notebook) {
+        AppDispatcher.dispatch({
+            actionType: NotebookConstants.REMOVE_NOTEBOOK,
+            notebook: notebook
+        });
+    },
+    createNotebook: function(notebook) {
+        AppDispatcher.dispatch({
+            actionType: NotebookConstants.CREATE_NOTEBOOK,
+            notebook: notebook
+        });
+    },
+    updateNotebook: function(notebook) {
+        AppDispatcher.dispatch({
+            actionType: NotebookConstants.UPDATE_NOTEBOOK,
+            notebook: notebook
+        });
+    }
+};
+
+module.exports = NotebookActions;
